d0afda2904f616472ed0fb67736b2a149ef4cb3c
Yuri_Ishitoya <yuri1121isty1206@gmail.com>
2023-05-26 15:59:33 +0900

    unsolved

--- not yet/problem026.c
+++ not yet/problem026.c
@@ -4,37 +4,71 @@
 #include <stdlib.h>
 #include <string.h>
 
+void checkCycle (double num, int* k, int* hantei2, int* count2) { 
+    // kは繰り返すかもしれないdigit, numは
+    int i=0;
+    double num2;
+
+    while(k[i] != '\0'){
+        num = (num - (double)k[0]) * 10.0;
+        if(k[i] != (int)num){
+            *hantei2=1;
+            break;
+        }
+        i++;
+    }
+    *count2 = i;
+}
+
 int main(){
-    int i, j, keta[100], lar=1;
+    int i, j, keta[100], lar=1, hantei=0, count=0;
     double q, k, d=10.0;
 
     for(i=2; i<=10; i++){
         q = 1.0/(double)i;
         
-        // 少数第2位
-        k = q * 10.0; // 10^-1の位の数を見る
+        // 少数第1位
+        k = q * d; // 10^-1の位の数を見る
         keta[0] = (int)k;
 
         // 少数第2位
-        k = (k - (double)keta[0])* 100.0;
+        k = (k - (double)keta[0])* d;
         keta[1] = (int)k;
 
-        if(keta[0]==keta[1]){
-            for(j=1; j<10; j++){
-                keta[j+1] = (k - (double)keta[0]) * d; // 次の桁
-                if(keta[0] != keta[j+1]) break;
-            }
-        } else {
-            for(j=1; j<10; j++){
-                keta[j+1] = (k - (double)keta[0]) * d; // 次の桁
-                if(keta[0] == keta[j+1]){
-                    for(k=j+1; k<j; k++){
-                        if(keta[k] != keta[k+1]) break;
-                    }
+        if(keta[0]==keta[1]){ // 1文字サイクル
+            for(j=2; j<10; j++){ // 10回続くか確かめる
+                k = (k - (double)keta[0]) * d;
+                keta[j] = (int)k; // 次の桁
+                if(keta[0] != keta[j]){
+                    hantei=1;
+                    break;
                 }
             }
+            if(hantei==0){
+                printf("one digit cycle -- ");
+            } else {
+                printf("not 1 digit cycle -- ");
+            }
+            hantei=0;
+        } else { // 複数文字サイクル
+            for(j=2; j<10; j++){ // keta[0]がもう一回出るまでketaに格納
+                k = (k - (double)keta[0]) * d;
+                keta[j] = (int)k; // 次の桁
+                if(keta[0] == keta[j]){ // keta[0]が~keta[j]まで繰り返しているかみる
+                    // ketaがその後繰り返されるかみる
+                    printf(" 関数　");
+                    checkCycle(k, keta, &hantei, &count);
+                    if(hantei==0){ // 繰り返していることがわかった
+                        printf("some digit cycle: %d-- ", count);
+                    } else { // 繰り返していない
+                        printf("not some digit cycle -- ");
+                    }
+                } 
+            }
+            if(hantei==0){ // 繰り返していることがわかった
+                printf("not some digit cycle -- ");
+            }
         }
-
-        printf("%d:%f,%f, keta:%d,%d\n", i,q,k, keta[0], keta[1]);
+        printf("%d:%f, keta:%d,%d\n", i,q, keta[0], keta[1]);
     }
 }
---
+++ not yet/problem027.c
@@ -0,0 +1,20 @@
+// Find the product of the coefficients, and , for the quadratic expression that produces the maximum number of primes for consecutive values of , starting with 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(){
+    //|a|も|b|も<1000で,n^2 + an + bでn=0~の時,連続して素数が生成されるかどうかを調べ,nが最大になるようなaとbを見つける
+    int i, j, hantei=0;
+
+    // |a|<100, |b|<=100でやってみる
+    while(hantei==0){
+        for(i=0; i<100; i++){
+            for(j=0; j<=100; j++){
+                
+            }
+        }      
+    }
+    // 素数かどうかの判定ー＞割れてしまうケース： n+a=b
+
+}
